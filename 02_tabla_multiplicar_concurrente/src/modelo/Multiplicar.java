038d47f39cedeeb354ce2a11c3a920e9552b364d
manana <manana@Core11-41>
2018-01-29 12:55:48 +0100

    02_tabla_multiplicar_concurrente V1

---
+++ 02_tabla_multiplicar_concurrente/src/modelo/Multiplicar.java
@@ -0,0 +1,22 @@
+package modelo;
+
+public class Multiplicar extends Thread {
+	private int numero;
+	public Multiplicar(int numero) {
+		this.numero=numero;
+	}
+	@Override
+	public void run() {
+		for(int i = 1; i<10;i++) {
+			System.out.println(this.numero + "x" + i + " = " + (this.numero*i));
+			try {
+				this.sleep(50);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}	
+	
+}
